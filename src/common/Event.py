01f6ae1bb1394ee6451650a6bf27ae312ef07c84
joshbyrom <joshbyrom@gmail.com>
2012-12-29 08:55:36 -0500

    created Event and Handler classes

--- src/common/Event.py
+++ src/common/Event.py
@@ -1,4 +1,12 @@
+import time
 
+class Event(object):
+    def __init__(self, message_type, message, reason):
+        self.message_type = message_type
+        self.message = message
+
+        self.timestamp = time.time()
+        
 
 
 if __name__ == '__main__':
--- src/common/Handler.py
+++ src/common/Handler.py
@@ -1,5 +1,18 @@
+from PubSub import *
 
+class Handler(PubSub):
+    def __init__(self, message_type, fun):
+        PubSub.__init__(self)
 
+        self.message_type = message_type
+        
+        self.fun = fun
+        self.call_count = 0
+
+    def handle(self, event, **kwargs):
+        if self.message_type == event.message_type:
+            self.fun(self.event, **kwargs)
+            self.call_count = self.call_count + 1
 
 if __name__ == '__main__':
     pass
