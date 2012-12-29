5221f1a03e1d16c58f0abe9bfc24cf0780ef69d4
joshbyrom <joshbyrom@gmail.com>
2012-12-29 10:31:06 -0500

    event records reason; handler emits callback event

--- src/common/Event.py
+++ src/common/Event.py
@@ -1,12 +1,12 @@
 import time
 
 class Event(object):
-    def __init__(self, message_type, message, reason):
+    def __init__(self, message_type, message='', reason=None):
         self.message_type = message_type
         self.message = message
 
         self.timestamp = time.time()
-        
+        self.reason = reason
 
 
 if __name__ == '__main__':
--- src/common/Handler.py
+++ src/common/Handler.py
@@ -1,18 +1,21 @@
 from PubSub import *
+from Event import *
 
 class Handler(PubSub):
-    def __init__(self, message_type, fun):
+    def __init__(self, message_type, callback):
         PubSub.__init__(self)
 
         self.message_type = message_type
         
-        self.fun = fun
+        self.callback = callback
         self.call_count = 0
 
     def handle(self, event, **kwargs):
         if self.message_type == event.message_type:
-            self.fun(self.event, **kwargs)
+            self.callback(self.event, **kwargs)
             self.call_count = self.call_count + 1
+
+            self.emit(Event('callback'))
 
 if __name__ == '__main__':
     pass
