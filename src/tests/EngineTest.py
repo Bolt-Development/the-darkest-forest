3d76298830508c851935da091f84eb9806b65c69
joshbyrom <joshbyrom@gmail.com>
2012-12-29 14:39:07 -0500

    EngineTest now shows sample engine use

--- src/tests/EngineTest.py
+++ src/tests/EngineTest.py
@@ -8,7 +8,13 @@ if __name__ == '__main__':
         sys.path.insert(0, up_one_folder)
 
     from models.Engine import *
+
+    def on_event(event, **kwargs):
+        print event, kwargs
+    
     
     engine = Engine()
     engine.set_caption('Engine Debug Testing')
+    engine.on('tick', on_event)
+    engine.on('render', on_event)
     engine.start()
