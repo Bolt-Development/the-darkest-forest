bc6e8b58f8e611ffe070a40211b165f77175245b
joshbyrom <joshbyrom@gmail.com>
2012-12-29 22:17:19 -0500

    created tile contents class

--- src/models/space/TileContents.py
+++ src/models/space/TileContents.py
@@ -3,7 +3,22 @@ from Tile import *
 
 
 class TileContents(PubSub):
-    def __init__(self
+    def __init__(self, tile):
+        PubSub.__init__(self)
+
+        self.tile = tile
+        self.contents = []
+
+    def add_content(self, content):
+        if content in self.contents:
+            self.contents.append(content)
+            self.emit('added', added = content)
+
+
+    def remove_content(self, content):
+        if content in self.contents:
+            self.contents.remove(content)
+            self.emit('removed', removed = content)
 
 
 if __name__ == '__main__':
