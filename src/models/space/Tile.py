0ef7acfdc9bd22aff4d6f61a5cdea76a0c357d64
joshbyrom <joshbyrom@gmail.com>
2012-12-29 22:13:05 -0500

    beginnings of tile class

--- src/models/space/Tile.py
+++ src/models/space/Tile.py
@@ -1,4 +1,13 @@
+# TODO :: import pubsub
 
+class Tile(PubSub):
+    def __init__(self, column, row, width, height):
+        self.column = column
+        self.row = row
+        self.width = width
+        self.height = height
+
+        # TODO :: add global position variable
 
 
 if __name__ == '__main__':
