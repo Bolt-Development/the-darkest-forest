b6c56f2aabb293af906cdcf9a10a72abcc465caa
joshbyrom <joshbyrom@gmail.com>
2012-12-31 12:16:35 -0500

    source path is only added once

--- src/tests/RelativePathTest.py
+++ src/tests/RelativePathTest.py
@@ -3,7 +3,7 @@ from TestingUtils import *
 if __name__ == '__main__':
     add_source_folder()
 
-
+    print sys.path
     # create a deep module
     from common.math.Collision import *
 
--- src/tests/TestingUtils.py
+++ src/tests/TestingUtils.py
@@ -2,4 +2,7 @@ import os
 import sys
 
 def add_source_folder():
-    sys.path.insert(0, os.path.normpath(os.path.join(os.getcwd(), "..")))
+    path = os.path.normpath(os.path.join(os.getcwd(), ".."))
+
+    if path not in sys.path:
+        sys.path.insert(0, path)
