45499363892404605df033760c619d795f2846f1
joshbyrom <joshbyrom@gmail.com>
2013-01-01 00:41:25 -0500

    restoring changes

--- src/controllers/AudioControllers.py
+++ src/controllers/AudioControllers.py
@@ -2,18 +2,11 @@ from models.AudioModels import *
 
 class AudioController(object):
     """Controls two channels of audio"""
-    def __init__(self):
-        channel_1 = pygame.mixer.Channel(0)
-        channel_2 = pygame.mixer.Channel(1)
-        self.channels = [channel_1, channel_2]
-        self.queue = []
-        self.loaded_files = {}
+    def __init__(self, model):
+        self.model = model
+    def play(self):
+        self.model.sound.play()
 
-    def play(self, filename):
-        if filename not in self.loaded_files:
-            model = AudioModel(filename)
-            self.loaded_files[filename] = model
-        self.loaded_files[filename].sound.play()
 
 
 
--- src/tests/AudioTest.py
+++ src/tests/AudioTest.py
@@ -2,21 +2,19 @@ from TestingUtils import *
 
 if __name__ == '__main__':
     add_source_folder()
+    add_resource_folder()
 
     from models.Engine import *
     from models.AudioModels import *
     from controllers.AudioControllers import *
+    from ResourceLoader import *
 
-    def on_event(event, **kwargs):
-        print event, kwargs
+    resource = ResourceLoader().load_resource_by_name_and_type('bomb', 'audio')
 
     def init_audio(event, **kwargs):
-        audio = AudioController()
+        audio = AudioController(AudioModel(resource))
         engine.on("w_down", audio.play)
-
+        
     engine = Engine()
     engine.on("init", init_audio, memory=True)
-    engine.on('tick', on_event)
-    engine.on('render', on_event)
     engine.start()
-
