ff403ee8f6cb2ab1a48a3b0329e05d6cbafd1338
joshbyrom <joshbyrom@gmail.com>
2013-01-01 12:23:28 -0500

    sprite sheet is rendered by test

--- src/tests/SpriteSheetTest.py
+++ src/tests/SpriteSheetTest.py
@@ -4,8 +4,20 @@ class SpriteSheetView(object):
     def __init__(self, model):
         self.model = model
 
+        self.x, self.y = 0, 0
+        self.scale_x = 1.0
+        self.scale_y = 1.0
+        self.rotation = 0
+
     def on_render(self, event, **kwargs):
         surface = kwargs['surface']
+
+        width = self.model.surface.get_width() * self.scale_x
+        height = self.model.surface.get_height() * self.scale_y
+
+        pygame.Surface.blit(surface,
+                            self.model.surface,
+                            [self.x, self.y, width, height])
 
         
 if __name__ == '__main__':
