df09a163288a06c1b500b2d18a6924efd86974ee
joshbyrom <joshbyrom@gmail.com>
2012-12-31 21:12:47 -0500

    created image model and resource loader classes

--- src/tests/TestingUtils.py
+++ src/tests/TestingUtils.py
@@ -2,7 +2,11 @@ import os
 import sys
 
 def add_source_folder():
-    path = os.path.normpath(os.path.join(os.getcwd(), ".."))
+    add_to_path(os.path.normpath(os.path.join(os.getcwd(), "..")))
 
+def add_to_path(path):
     if path not in sys.path:
         sys.path.insert(0, path)
+
+def add_resource_folder():
+    add_to_path(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..//..', 'res')))
