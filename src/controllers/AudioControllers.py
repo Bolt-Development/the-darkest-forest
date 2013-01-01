33bce9ea2ab44aeeea6ef5dc4fa2a538a9178cfb
joshbyrom <joshbyrom@gmail.com>
2013-01-01 00:43:32 -0500

    re-added audio channel controller

--- src/controllers/AudioControllers.py
+++ src/controllers/AudioControllers.py
@@ -7,6 +7,21 @@ class AudioController(object):
     def play(self):
         self.model.sound.play()
 
+class AudioChannelController(object):
+    def __init__(self):
+        channel_1 = pygame.mixer.Channel(0)
+        channel_2 = pygame.mixer.Channel(1)
+        self.channels = [channel_1, channel_2]
+        self.queue = []
+        self.loaded_files = {}
+
+    def play(self, filename):
+        if filename not in self.loaded_files:
+            model = AudioModel(filename)
+            self.loaded_files[filename] = model
+        self.loaded_files[filename].sound.play()
+
+
 
 
 
