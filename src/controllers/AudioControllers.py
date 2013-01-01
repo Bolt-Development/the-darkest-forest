916d50d112964f74c751b3dd7fc1425a12987ac3
Joshua Rowe <joshuarowe1@me.com>
2013-01-01 16:07:17 +1100

    Trying to play specific file

--- src/controllers/AudioControllers.py
+++ src/controllers/AudioControllers.py
@@ -1,11 +1,19 @@
 from models.AudioModels import *
 
 class AudioController(object):
-    """Will eventually deal with 1 channel of audio, instead of just 1 audio file."""
-    def __init__(self, model):
-        self.model = model
-    def play(self):
-        self.model.sound.play()
+    """Controls two channels of audio"""
+    def __init__(self):
+        channel_1 = pygame.mixer.Channel(0)
+        channel_2 = pygame.mixer.Channel(1)
+        self.channels = [channel_1, channel_2]
+        self.queue = []
+        self.loaded_files = {}
+
+    def play(self, filename):
+        if filename not in self.loaded_files:
+            model = AudioModel(filename)
+            self.loaded_files[filename] = model
+        self.loaded_files[filename].sound.play()
 
 
 
