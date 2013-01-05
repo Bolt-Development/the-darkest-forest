d1123548937342c9b57d6953cc7bb84c22271a8e
Joshua Rowe <joshuarowe1@me.com>
2013-01-06 00:37:30 +1100

    code needs fixing I think, I'm unsure of how to start on Scene

--- src/tests/SceneTest.py
+++ src/tests/SceneTest.py
@@ -7,14 +7,15 @@ if __name__ == '__main__':
     from common.Scene import *
 
 
-    def print_event(event, **kwards):
+    def print_event(event, **kwargs):
         print event
 
     intro_scene = Scene(None)
     child_scene = Scene(intro_scene)
     intro_scene.add_child(child_scene)
 
-
+    # I think I'm doing something wrong here, structurally. And in Scene.
+    # I'll work on something else until you get back and can help me.
     intro_scene.engine.on("on_child_added", print_event)
     intro_scene.engine.start()
 
