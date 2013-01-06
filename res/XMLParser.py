138fa2cae98cd3e247cd3dcaa4fe2444c19c0361
Joshua Rowe <joshuarowe1@me.com>
2013-01-06 15:58:14 +1100

    Added a small test in XMLParser

--- res/XMLParser.py
+++ res/XMLParser.py
@@ -1,13 +1,24 @@
+import xml.etree.ElementTree as ET
+
 class XMLParser(object):
 
     def get_root_element(self, filepath):
         pass
     def parse(self, filepath, type_name):
         """
-        def parse(self, filepath, type_name):
+        parse(filepath, type_name)
         filepath = path of .xml file
         type_name = name of the enemy, item, etc that you will be extracting
         Return:
         -> Will return a dictionary containing the attributes contained within the element root_name
         """
-        pass
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+        print root.tag
+
+if __name__ == '__main__':
+    from ResourceLoader import *
+
+    xml_resource = ResourceLoader().load_resource_by_name_and_type('enemies', 'other')
+
+    XMLParser().parse(xml_resource.path, "SuperBadGuy")
