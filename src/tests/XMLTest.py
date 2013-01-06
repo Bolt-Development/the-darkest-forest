5b6be6d5025c3875f2df1bdfd0875cb94ab77c94
Joshua Rowe <joshuarowe1@me.com>
2013-01-06 16:45:09 +1100

    Updated XMLTest

--- src/tests/XMLTest.py
+++ src/tests/XMLTest.py
@@ -5,5 +5,10 @@ if __name__ == '__main__':
     add_resource_folder()
 
     from ResourceLoader import *
+    from XMLParser import *
 
-    xml_resource = ResourceLoader().load_resource_by_name_and_type('monster', 'other')
+    xml_resource = ResourceLoader().load_resource_by_name_and_type('enemies', 'enemy')
+
+    attributes = XMLParser().parse(xml_resource, "SuperScaryGuy")
+    print "hp:", attributes["hp"]
+    print "damage:", attributes["damage"]
