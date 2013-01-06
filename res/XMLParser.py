287036fd74aa1d885cb671370f12a9822e061ae9
Joshua Rowe <joshuarowe1@me.com>
2013-01-06 15:51:32 +1100

    modified xml to adhere to Data Types guidelines

--- res/XMLParser.py
+++ res/XMLParser.py
@@ -1,4 +1,13 @@
 class XMLParser(object):
 
-    def get_root_element(self):
+    def get_root_element(self, filepath):
+        pass
+    def parse(self, filepath, type_name):
+        """
+        def parse(self, filepath, type_name):
+        filepath = path of .xml file
+        type_name = name of the enemy, item, etc that you will be extracting
+        Return:
+        -> Will return a dictionary containing the attributes contained within the element root_name
+        """
         pass
