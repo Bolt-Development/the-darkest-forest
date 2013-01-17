377114c9ff9e0146ed575e87c2d50f47bc3d356f
joshbyrom <joshbyrom@gmail.com>
2013-01-17 11:37:22 -0500

    structure for additional GUI elements

--- src/common/gui/Buttons.py
+++ src/common/gui/Buttons.py
@@ -13,11 +13,12 @@ class Button(ElementView):
         self._offset_x = 10
         self._offset_y = 3
         
-        self._text_view = TextView(text_model)
-        self._text_view.x = self._offset_x
-        self._text_view.y = self._offset_y
+        if text_model is not None:
+            self._text_view = TextView(text_model)
+            self._text_view.x = self._offset_x
+            self._text_view.y = self._offset_y
         
-        self.add_child(self._text_view)
+            self.add_child(self._text_view)
         
         self.show_background = True
         self.show_border = True
@@ -49,3 +50,26 @@ class Button(ElementView):
     def get_text(self):
         return self._text_view.text
     text = property(get_text, set_text)
+    
+class Toggle(Button):
+    def __init__(self, width, height, active=False):
+        Button.__init__(self, None)
+        
+        self._width = width
+        self._height = height
+        
+        self._active = active
+        
+    def on_mouse_clicked(self, event, **kwargs):
+        self.active = not self._active
+        
+    def _get_active(self):
+        return self._active
+    def _set_active(self, value):
+        if value != self._active:
+            if value:
+                self.emit('on')
+            else:
+                self.emit('off')
+        self._active = value
+    active = property(_get_active, _set_active)
